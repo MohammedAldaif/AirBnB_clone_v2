1cbf55fdb60bc6a231fb8db99b4ba30b8d4c66db
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-04-19 23:34:08 -0700

    M

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+this module prints HBNB when /hbnb page is requested
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """
+    prints c followed by text string
+    """
+    text = text.replace("_", " ")
+    return f'C {escape(text)}'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """
+    this function print HBNB when /hbnb page is requested
+    Return: string
+    """
+    return "HBNB"
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """
+    this functuon prints Hello HBNB! when / page is requested
+    Return: string
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
