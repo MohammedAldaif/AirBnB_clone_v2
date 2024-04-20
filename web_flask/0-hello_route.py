fcd1e612097b31469fc79b18c176e53c5df81afc
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-04-19 22:36:21 -0700

    M

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+""" this module prints a simple string using flask """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/",strict_slashes=False)
 def hello_world():
-    return "<p>Hello HBNB!</p>"
+    """
+    This function returns a simple greeting message.
+
+    Returns:
+        str: A string saying "Hello HBNB!".
+    """
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
